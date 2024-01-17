50359c63754ab723be7efd94311514a96f0dbe49
Kris Alavattam <34422552+kalavattam@users.noreply.github.com>
2024-01-16 21:23:40 -0500

    Adding code, plots, tables

--- Fig-3E.R
+++ Fig-3E.R
@@ -3,16 +3,35 @@
 #  Fig-3E.R
 #  KA
 
+#  Determine the directory the script is being run from
+dir_script <- if (
+    interactive() && requireNamespace("rstudioapi", quietly = TRUE)
+) {
+    dirname(rstudioapi::getSourceEditorContext()$path)
+} else {
+    args <- commandArgs(trailingOnly = FALSE)
+    arg_script <- args[grep("^--file=", args)]
+    path_script <- sub("^--file=", "", arg_script[length(arg_script)]) 
+    normalizePath(path_script, mustWork = FALSE)
+    dirname(path_script)
+}
+
 #  Load necessary libraries
-library(googlesheets4)
 library(tidyverse)
 library(scales)
 
-#  Read in Google Sheets sheet "yH2AX_all"
-URL_pre <- "https://docs.google.com/spreadsheets/d"
-URL_suf <- "1mjOWHt3MS4rwYOlr0aiIeIeHrIBvzUSPBd6GnjuXES4/edit"
-URL <- paste(URL_pre, URL_suf, sep = "/")
-data <- googlesheets4::read_sheet(URL, sheet = "SYCP1_auto_observations")
+#  Load in data from TSV file; files of interest are stored in the base
+#+ directory of the repo, which is the same location
+data <- readr::read_tsv(
+    file.path(
+        dir_script, "tsvs",
+        "wrangle-assess_blinded-scoring_Atf7ip2.SYCP1_auto_observations_run-3.tsv"
+    ),
+    show_col_types = FALSE
+) %>%
+    
+    #  Convert the proportions to numeric format (if it's not already)
+    dplyr::mutate(proportion = as.numeric(gsub("%", "", proportion)) / 100)
 
 #  Further wrangle the data, calculating total sample sizes along with the
 #+ summary statistics SD and SEM
@@ -68,7 +87,7 @@ save_plots <- TRUE
 if (isTRUE(save_plots)) {
     #  Save the plot with SD bars (will be saved to $HOME directory)
     ggplot2::ggsave(
-        "Fig-3E_prop-SYCP1-auto_SD.pdf",
+        "Fig-3E_prop-SYCP1-auto_run-2_SD.pdf",
         plot = plot_sd,
         width = 8.5,
         height = 6,
@@ -77,7 +96,7 @@ if (isTRUE(save_plots)) {
     
     #  Save the plot with SEM bars (will be saved to $HOME directory)
     ggplot2::ggsave(
-        "Fig-3E_prop-SYCP1-auto_SEM.pdf",
+        "Fig-3E_prop-SYCP1-auto_run-2_SEM.pdf",
         plot = plot_sem,
         width = 8.5,
         height = 6,
@@ -113,4 +132,6 @@ if (isTRUE(check_p_values)) {
 }
 
 #  The p-values from t-tests are as follows:
-#+ - P   0.2208883
+#+ - P 0.2208883 (run 1)
+#+ - P 0.1970768 (run 2)
+#+ - P 0.009593572 (run 3)
